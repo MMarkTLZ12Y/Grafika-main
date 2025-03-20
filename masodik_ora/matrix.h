a1b4dedbec4a7723f15b044b71583c9d40b10a5c
MMark <146880976+MMarkTLZ12Y@users.noreply.github.com>
2025-03-20 14:36:10 +0100

    egyseg_matrix_beepitese

---
+++ masodik_ora/matrix.h
@@ -0,0 +1,27 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+/**
+ * Initializes all elements of the matrix to zero.
+ */
+void init_zero_matrix(float matrix[3][3]);
+
+/**
+ * Initializes all elements of the matrix to zero except main diagonal; main diagonal=1.
+
+ */
+ void initIdentityMatrix(float matrix[3][3]);
+
+
+/**
+ * Print the elements of the matrix.
+ */
+void print_matrix(const float matrix[3][3]);
+
+/**
+ * Add matrices.
+ */
+void add_matrices(const float a[3][3], const float b[3][3], float c[3][3]);
+
+#endif // MATRIX_H
+
